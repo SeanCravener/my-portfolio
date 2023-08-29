461776eec6f19d938d2df4223ccb88d4749ebb2c
Sean Cravener <scravenerpat@gmail.com>
2023-08-29 14:41:18 -0400

    Window file setup to load context and apply type.

---
+++ src/components/Window.jsx
@@ -0,0 +1,61 @@
+import { useContext } from "react";
+import { WindowContext } from "./WindowContext";
+import { Modal, List } from "@react95/core";
+import Explorer from "./Explorer";
+import IframeViewer from "./IframeViewer";
+import Notepad from "./Notepad";
+
+export default function Window({ selectedWindow }) {
+  const { closeWindow } = useContext(WindowContext);
+
+  const handleCloseWindow = () => {
+    closeWindow(selectedWindow);
+  };
+
+  const typeSelector = () => {
+    switch (selectedWindow.type) {
+      case "folder-explorer":
+        return <Explorer content={selectedWindow.content} />;
+      case "iframe-viewer":
+        return <IframeViewer content={selectedWindow} />;
+      case "notepad":
+        console.log(selectedWindow.content);
+        return <Notepad content={selectedWindow.content} />;
+      default:
+        return console.log("Error: Window type not found.");
+    }
+  };
+
+  const populateMenu = (menu) => {
+    return [
+      {
+        name: "Links",
+        list: (
+          <List>
+            {menu.map((item, index) => {
+              return (
+                <List.Item key={index}>
+                  <a href={item.link} target="_blank" rel="noreferrer">
+                    {item.name}
+                  </a>
+                </List.Item>
+              );
+            })}
+          </List>
+        ),
+      },
+    ];
+  };
+
+  return (
+    <Modal
+      title={selectedWindow.title}
+      defaultPosition={{ x: 100, y: 100 }}
+      closeModal={handleCloseWindow}
+      menu={selectedWindow.menu && populateMenu(selectedWindow.menu)}
+      buttons={[{ value: "Close", onClick: handleCloseWindow }]}
+    >
+      {typeSelector()}
+    </Modal>
+  );
+}
